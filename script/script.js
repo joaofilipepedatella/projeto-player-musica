75dd5b69f99fbc7ab37cba82c8d0b837ecce0b99
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-01-13 19:56:13 -0300

    criacao das funcoes de nextSong() e previousSong() e funcao de atualizacao das informacoes que sao mostradas ao ouvinte

--- script/script.js
+++ script/script.js
@@ -1,10 +1,34 @@
 const songName = document.querySelector("#song-name")
+const bandName = document.querySelector("#band-name")
 const song = document.querySelector("#audio")
+const cover = document.querySelector("#cover")
 const play = document.querySelector("#play")
+const next = document.querySelector("#next")
+const previous = document.querySelector("#previous")
 
-songName.innerText = "As you were"
+const asYouWere = {
+    songName: "As You Were",
+    artist: "TrackTribe",
+    file: "as_you_were"
+}
+const boomBapFlick = {
+    songName: "Boom Bap Flick",
+    artist: "Quincas Moreira",
+    file: "boom_bap_flick"
+}
+const cantHide = {
+    songName: "Can't Hide", /* 'can\'t hide'  => caso de aspas simples*/
+    artist: "Otis McDonald",
+    file: "cant_hide"
+}
 
 let isPlaying = false
+
+const playlist = [asYouWere, boomBapFlick, cantHide]
+
+let index = 0
+
+
 
 function playSong() {
     play.querySelector(".bi").classList.remove("bi-play-circle-fill")
@@ -21,11 +45,43 @@ function pauseSong() {
 }
 
 function playPauseDecider() {
-    if( isPlaying === true ) {
+    if (isPlaying === true) {
         pauseSong()
     } else {
         playSong()
     }
 }
 
+function initializeSong(){
+    cover.src = `imagens/${playlist[index].file}.webp`
+    song.src = `songs/${playlist[index].file}.mp3`
+    songName.innerText = playlist[index].songName
+    bandName.innerText = playlist[index].artist
+}
+
+function previousSong() {
+    if( index === 0){
+        index = playlist.length - 1
+    } else {
+        index -= 1
+    }
+
+    initializeSong()
+    playSong()
+}
+
+function nextSong() {
+    if( index === playlist.length - 1){
+        index = 0
+    } else {
+        index += 1
+    }
+
+    initializeSong()
+    playSong()
+}
+initializeSong()
+
 play.addEventListener('click', playPauseDecider)
+previous.addEventListener('click', previousSong)
+next.addEventListener('click', nextSong)
