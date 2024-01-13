0d87baeafba87f9082b7f7d800ff7c21a4efd423
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-01-13 19:17:22 -0300

    Adicinada pasta com musicas, troca das imagens, feita funcao de dar play/pause e seu evento

---
+++ script/script.js
@@ -0,0 +1,31 @@
+const songName = document.querySelector("#song-name")
+const song = document.querySelector("#audio")
+const play = document.querySelector("#play")
+
+songName.innerText = "As you were"
+
+let isPlaying = false
+
+function playSong() {
+    play.querySelector(".bi").classList.remove("bi-play-circle-fill")
+    play.querySelector(".bi").classList.add("bi-pause-circle-fill")
+    song.play()
+    isPlaying = true
+}
+
+function pauseSong() {
+    play.querySelector(".bi").classList.remove("bi-pause-circle-fill")
+    play.querySelector(".bi").classList.add("bi-play-circle-fill")
+    song.pause()
+    isPlaying = false
+}
+
+function playPauseDecider() {
+    if( isPlaying === true ) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+}
+
+play.addEventListener('click', playPauseDecider)
